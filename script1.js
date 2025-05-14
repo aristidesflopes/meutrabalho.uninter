eeaf5b24951f27be8418b940947f45571f15e54f
aristidesflopes <aristides.lopes@hotmail.com>
2025-05-14 14:42:09 -0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,8 @@
+let botao = document.getElementById("botao");
+
+let mensagemDiv = document.getElementById("mensagem");
+
+botao.addEventListener("click", function() {
+mensagemDiv.textContent = "Não é a carga que o derruba, mas a maneira como você a carrega.!";
+mensagemDiv.style.display = "block"; 
+                                             });
